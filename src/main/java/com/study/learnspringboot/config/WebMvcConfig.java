2f4c3126796a5e9c91ae835765ae2442f3a99e32
xjWen <2899807359@qq.com>
2018-11-27 10:54:46 +0800

    2018/11/27

--- src/main/java/com/study/learnspringboot/config/WebMvcConfig.java
+++ src/main/java/com/study/learnspringboot/config/WebMvcConfig.java
@@ -4,6 +4,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+/**
+ * SpringMVC config class
+ *
+ * @author xjwen
+ * @date 2018/11/27
+ */
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
